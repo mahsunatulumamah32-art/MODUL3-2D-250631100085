5f3a667454b8955e29af93853d0a85a233d316bb
mahsunatulumamah32-art <mahsunatulumamah32@gmail.com>
2026-02-27 10:01:34 +0700

    TUGAS_PBO_03

---
+++ student.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */package student;
+public class student {
+
+    String name;
+    int age;
+    String city;
+
+    void introduce() {
+    System.out.println("Hi, my name is " + name + " and I am " + age + " years old " + "i from " + city);
+    }
+}
